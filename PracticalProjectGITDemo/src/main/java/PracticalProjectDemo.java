cf09fc01f4607abdea3dab7180c5386b6a85624d
Bogdan <vasilebogdanf@gmail.com>
2022-09-25 11:58:39 +0300

    project stopped

--- PracticalProjectGITDemo/src/main/java/PracticalProjectDemo.java
+++ PracticalProjectGITDemo/src/main/java/PracticalProjectDemo.java
@@ -1,5 +1,7 @@
 public class PracticalProjectDemo {
     public static void main(String[] args) {
+
         System.out.println("Practical Project");
+        System.out.println("Practical Project stoppesd");
     }
 }
