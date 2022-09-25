825603902ab5c38ba1f4e66970952316448ccc4c
Bogdan <vasilebogdanf@gmail.com>
2022-09-25 12:03:31 +0300

    commit 3

--- PracticalProjectGITDemo/src/main/java/PracticalProjectDemo.java
+++ PracticalProjectGITDemo/src/main/java/PracticalProjectDemo.java
@@ -2,6 +2,6 @@ public class PracticalProjectDemo {
     public static void main(String[] args) {
 
         System.out.println("Practical Project");
-        System.out.println("Practical Project stoppesd");
+        System.out.println("Practical Project stopped");
     }
 }
